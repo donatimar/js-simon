b2d5897e9273e99ec523666e0b6f6dba7a2d36d0
donatimar <donatimar@gmail.com>
2024-10-24 16:56:10 +0200

    added 5 random number

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+const generateButton = document.getElementById("generateButton");
 
+generateButton.addEventListener("click", function () {
+  const randomNumbers = [];
+
+  while (randomNumbers.length < 5) {
+    const randomNumber = Math.floor(Math.random() * 99) + 1;
+
+    if (!randomNumbers.includes(randomNumber)) {
+      randomNumbers.push(randomNumber);
+    }
+  }
+
+  const numberList = document.getElementById("randomNumbers");
+  numberList.innerHTML = "";
+
+  randomNumbers.forEach((num) => {
+    const listItem = document.createElement("li");
+    listItem.className = "list-group-item";
+    listItem.textContent = num;
+    numberList.appendChild(listItem);
+  });
+});
